285fe0111fdaa4770ebf2131fb4c39a39fe0d698
EslamKamel89 <elsamkamelforex@gmail.com>
2024-10-24 21:38:40 +0300

    first commit

---
+++ app/Http/Controllers/GoldPriceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GoldPrice;
+use App\Http\Requests\StoreGoldPriceRequest;
+use App\Http\Requests\UpdateGoldPriceRequest;
+
+class GoldPriceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGoldPriceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GoldPrice $goldPrice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GoldPrice $goldPrice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGoldPriceRequest $request, GoldPrice $goldPrice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GoldPrice $goldPrice)
+    {
+        //
+    }
+}
